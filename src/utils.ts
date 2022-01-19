dccfa10132f94f4ac9cc4863db88992e35deb653
Steven Obua <steven@recursivemind.com>
2022-01-19 07:33:37 +0000

    v0.0.13

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,3 @@
-import { combineHashCodes, Hashables } from "./hashable";
-import { int, nat, numbers } from "./primitives";
-
-export function isInt(x : any) : x is int {
-    return Number.isInteger(x);
-}
-
-export function isNat(x : any) : x is nat {
-    return Number.isInteger(x) && x >= 0;
-}
-
-export function assertNat(n : any): asserts n is number {
-    if (!(Number.isInteger(n) && n >= 0)) {
-        throw new TypeError(`natural number expected, found: ${n}`);
-    }
-}
-
-export function assertInt(n : any): asserts n is number {
-    if (!Number.isInteger(n)) {
-        throw new TypeError(`integer expected, found: ${n}`);
-    }
-}
-
 export function* iterateCodepoints(s : string) {
     for (let v of s) {
         yield v.codePointAt(0)!;
@@ -41,3 +18,4 @@ export function joinStrings(separator : string, strings : Iterable<string>) : st
     }
     return result;
 }
+
