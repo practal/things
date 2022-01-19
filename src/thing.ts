da3f844a0a9acbec653ccff2dac0bc7669941955
Steven Obua <steven@recursivemind.com>
2022-01-19 14:46:56 +0000

    Update thing.ts

--- src/thing.ts
+++ src/thing.ts
@@ -1,7 +1,7 @@
 import { Equatable } from "./equatable";
-import { Hashable } from "./hashable";
-import { Comparable } from "./comparable";
-import { int } from "./primitives";
+import { Hash, Hashable } from "./hashable";
+import { Comparable, PartialOrder } from "./comparable";
+import { bigints, booleans, int, numbers, strings } from "./primitives";
 
 export class Thing implements Equatable, Hashable, Comparable {
  
@@ -16,7 +16,7 @@ export class Thing implements Equatable, Hashable, Comparable {
     }
 
     get hash(): int {
-        return 0;
+        return strings.hash(this.toString());
     }
 
     compare(other: any): number {
@@ -32,3 +32,52 @@ export class Thing implements Equatable, Hashable, Comparable {
 export function finalClass(finalClass : string) : never {
     throw new Error(`Cannot subclass final class ${finalClass}.`);
 }
+
+export const Things: Hash<any> & PartialOrder<any> = {
+
+    equals(lhs : any, rhs : any) : boolean {
+        if (lhs instanceof Thing) {
+            return lhs.equals(rhs); 
+        } else if (rhs instanceof Thing) {
+            return rhs.equals(lhs);
+        } else {
+            return lhs === rhs || (Number.isNaN(lhs) && Number.isNaN(rhs));
+        }
+    },
+
+    hash(t : any) : int {
+        if (t instanceof Thing) {
+            return t.hash;
+        } else {
+            switch (typeof t) {
+                case "number": return numbers.hash(t);
+                case "string": return strings.hash(t);
+                case "boolean": return booleans.hash(t);
+                case "bigint": return bigints.hash(t);
+                default: return strings.hash(`${t}`);
+            }
+        }
+        throw new Error();
+    },
+
+    compare(lhs : any, rhs : any) : number {
+        if (lhs instanceof Thing) {
+            return lhs.compare(rhs); 
+        } else if (rhs instanceof Thing) {
+            return -rhs.compare(lhs);
+        } else {
+            if (lhs < rhs) return -1;
+            else if (lhs > rhs) return 1;
+            else if (lhs === rhs) return 0;
+            else return Number.NaN;
+        }
+    }
+}
+
+Object.freeze(Things);
+
+export function canonicalThings<T>() : Hash<T> & PartialOrder<T> {
+    return Things;
+}
+
+Object.freeze(canonicalThings);
