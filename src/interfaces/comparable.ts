d6e02ba55bb6cf60b57327c35cbc9492f3d3be29
Steven Obua <steven@recursivemind.com>
2022-01-20 16:22:00 +0000

    Update comparable.ts

--- src/interfaces/comparable.ts
+++ src/interfaces/comparable.ts
@@ -1,18 +1,16 @@
 import { Equatable, Equality } from "./equatable";
 
-export const UNRELATED = 0;
 export type UNRELATED = 0;
-
-export const LESS = 1;
 export type LESS = 1;
-
-export const EQUAL = 2;
 export type EQUAL = 2;
-
-export const GREATER = 3;
 export type GREATER = 3;
 
 export type ComparisonResult = UNRELATED | LESS | EQUAL | GREATER;
+
+export const UNRELATED : ComparisonResult = 0;
+export const LESS : ComparisonResult = 1;
+export const EQUAL : ComparisonResult = 2;
+export const GREATER : ComparisonResult = 3;
 
 export interface PartialOrder<T> extends Equality<T> {
 
@@ -27,7 +25,7 @@ export interface PartialOrder<T> extends Equality<T> {
      *   * compare(x, y) === LESS implies compare(y, x) === GREATER
      *   * compare(x, y) === GREATER implies compare(y, x) === LESS
      *   
-     *   From this it follows that compare(x, y) === UNRELATED implies compare(y, x) === UNRELATED. 
+     * From this it follows that compare(x, y) === UNRELATED implies compare(y, x) === UNRELATED. 
      */
     compare(lhs : T, rhs : T) : ComparisonResult
 
@@ -46,7 +44,7 @@ export interface Comparable extends Equatable {
      *   * this.compareTo(other) === LESS implies other.compareTo(this) === GREATER if other is a Comparable
      *   * this.compareTo(other) === GREATER implies other.compareTo(this) === LESS if other is a Comparable
      * 
-     *   From this it follows that this.compareTo(other) === UNRELATED implies other.compareTo(this) === UNRELATED if other is a Comparable.
+     * From this it follows that this.compareTo(other) === UNRELATED implies other.compareTo(this) === UNRELATED if other is a Comparable.
      */
     compareTo(other : any) : ComparisonResult
 
