3aaea87eb20aef96e7e04947ea9813c2d405162a
Steven Obua <steven@recursivemind.com>
2022-01-20 16:24:56 +0000

    Update map.ts

--- src/interfaces/map.ts
+++ src/interfaces/map.ts
@@ -10,9 +10,9 @@ export interface MutableMap<Key, Value> extends Map<Key, Value> {
 
 export interface ImmutableMap<Key, Value> extends ReadonlyMap<Key, Value> {
 
-    put(key : Key, value : Value) : {old : Value | undefined, result : ImmutableMap<Key, Value> }
+    put(key : Key, value : Value) : { old : Value | undefined, result : ImmutableMap<Key, Value> }
 
-    putIfUndefined(key : Key, value : Value) : {old : Value | undefined, result : ImmutableMap<Key, Value> }
+    putIfUndefined(key : Key, value : Value) : { old : Value | undefined, result : ImmutableMap<Key, Value> }
 
     remove(key : Key) : { old : Value | undefined, result : ImmutableMap<Key, Value> }
 
