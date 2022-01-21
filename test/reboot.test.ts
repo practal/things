cc6edcaf09d1895d269e747ea3ccdc4060d24099
Steven Obua <steven@recursivemind.com>
2022-01-21 19:05:56 +0000

    Update reboot.test.ts

--- test/reboot.test.ts
+++ test/reboot.test.ts
@@ -13,6 +13,7 @@ test("JavaScript Behaviour: reboot", () => {
     console.log("n.equals(c)", n.equals(c));
     console.log("n.equals(d)", n.equals(d));
     console.log("n.equals(e)", n.equals(e));
+    expect((1 as any) == "1").toBe(true);
 });
 
 test("JavaScript Behaviour: bigint", () => {
