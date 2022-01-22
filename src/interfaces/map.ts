7866038f667bfbcd57aa407bfe1a436f11198cd1
Steven Obua <steven@recursivemind.com>
2022-01-22 11:13:11 +0000

    MapThing

--- src/interfaces/map.ts
+++ src/interfaces/map.ts
@@ -1,4 +1,12 @@
-export interface MutableMap<Key, Value> extends Map<Key, Value> {
+import { Something, Things } from "./things";
+
+export type MapThing<Key, Value> = MutableMap<Key, Value> | ImmutableMap<Key, Value>
+
+export interface MutableMap<Key, Value> extends Map<Key, Value>, Something {
+
+    get Keys() : Things<Key>
+
+    get Values() : Things<Value>
 
     put(key : Key, value : Value) : Value | undefined
 
@@ -8,7 +16,11 @@ export interface MutableMap<Key, Value> extends Map<Key, Value> {
 
 }
 
-export interface ImmutableMap<Key, Value> extends ReadonlyMap<Key, Value> {
+export interface ImmutableMap<Key, Value> extends ReadonlyMap<Key, Value>, Something {
+
+    get Keys() : Things<Key>
+
+    get Values() : Things<Value>
 
     put(key : Key, value : Value) : { old : Value | undefined, result : ImmutableMap<Key, Value> }
 
