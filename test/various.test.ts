0d0990336bc76c72c7238c0346d922ae63b96182
Steven Obua <steven@recursivemind.com>
2022-01-26 07:06:42 +0000

    Renamed test files.

--- test/various.test.ts
+++ test/various.test.ts
@@ -1,96 +1,186 @@
-import {primitives} from "../src/index";
+import { Int, MutableInt, Nat, Num } from "../src/implementations/numberthing";
+import { AssocArray, AssocArrayFor } from "../src/implementations/assoc_array";
+import { HashMap, HashMapFor } from "../src/implementations/hashmap";
+import { int, nat } from "../src/interfaces/primitives";
+import { LESS, UNRELATED } from "../src/interfaces/comparable";
+import { MutableMap } from "../src/interfaces/map";
+import { numbers } from "../src/implementations/primitives";
+import { Anything } from "../src/implementations/anything";
 
-
-
-enum R {
-    LESS = -1,
-    EQUAL,
-    GREATER,
-    UNRELATED
+function randomNat(max : nat = Number.MAX_SAFE_INTEGER) : number {
+    return Math.round(Math.random() * max);
 }
 
-const LESS = -1;
+function coinflip() : boolean {
+    return Math.random() >= 0.5;
+}
 
-test("JavaScript Behaviour: enum", () => {
-    expect(R.LESS).toBe(-1);
-    (R as any).LESS = 42;
-    expect(R.LESS).toBe(42);
-    Object.freeze(R);
-    expect(() => {(R as any).LESS = 66;}).toThrow();
-    expect(R.LESS).toBe(42);
-    expect(LESS).toBe(-1);
-    expect(() => {(LESS as any) = 2;}).toThrow();
-    expect(LESS).toBe(-1);
-    expect(() => {(LESS as any).x = 7;}).toThrow();
-
-    function test() : -1 {
-        return LESS;
-    }
-
+test("immutable Nat", () => {
+    let n = new Nat(1);
+    expect(() => (n as any).value = 5).toThrow();
 });
 
-test("JavaScript Behaviour: undefined and null", () => {
-    expect(undefined === undefined).toBe(true);
-    expect(undefined == undefined).toBe(true);
-    let m = new Map();
-    m.set(undefined, 7);
-    m.set(undefined, 6);
-    expect(m.size).toBe(1);
-    expect(m.get(undefined)).toBe(6);
-    m.set(undefined, undefined);
-    expect(m.get(undefined)).toBe(undefined);
-    expect(m.size).toBe(1);
-
-    expect(0 == null).toBe(false);
-    expect(null == undefined).toBe(true);
-    expect(false == null).toBe(false);
-    expect(undefined == undefined).toBe(true);
-    expect((undefined as any) < (undefined as any)).toBe(false);
-    expect(null == null).toBe(true);
-    expect((null as any) < (null as any)).toBe(false);
-    expect((undefined as any) < 1).toBe(false);
-
-    expect(false < true).toBe(true);
-
-    expect((false as any) < 1).toBe(true);
-
-    expect((true as any) >= 1).toBe(true);
+test("equals Nat MutableInt number", () => {
+    let n = new Nat(7);
+    let m = new MutableInt(7);
+    let o = new MutableInt(8);
+    let x = 7;
+    expect(n.equals(o)).toBe(false);
+    expect(o.equals(n)).toBe(false);
+    expect(m.equals(o)).toBe(false);
+    expect(n.equals(m)).toBe(true);
+    expect(m.equals(n)).toBe(true);
+    expect(m.equals(x as any)).toBe(true);
+    expect(n.equals(new Nat(7))).toBe(true);
+    expect(n.equals(new Nat(8))).toBe(false);
+    expect(() => (x as any).equals(n)).toThrow();
 });
 
-test("JavaScript Behaviour: NaN", () => {
-    expect(Number.NaN < 0).toBe(false);
-    expect(-0 === 0).toBe(true);
-    expect(Number.NaN === Number.NaN).toBe(false);
-    expect(Number.NaN == Number.NaN).toBe(false);
-    expect(Number.NaN === undefined).toBe(false);
-    expect(Number.NaN == undefined).toBe(false);
+function testMutableMap(arr : MutableMap<int, int>) {
+    arr.assign([[1, 7], [3, 2], [3, 1]]);
+    console.log(`testMutableMap: ${arr}`);
+    expect(arr.size).toBe(2);
+    expect(arr.get(1)).toBe(7);
+    expect(arr.get(3)).toBe(1);
+    expect(arr.get(2)).toBeUndefined();
+    let brr = arr.clone();
+    expect(brr.size).toBe(2);
+    expect(brr.get(1)).toBe(7);
+    expect(brr.get(3)).toBe(1);
+    expect(brr.get(2)).toBeUndefined();
+    expect(arr.delete(2)).toBe(false);
+    expect(arr.delete(1)).toBe(true);
+    console.log(`arr = ${arr}, brr = ${brr}`);
+    expect(arr.size).toBe(1);
+    expect(brr.size).toBe(2);
+    expect(arr.isEqualTo(brr)).toBe(false);
+    arr.put(1, 7);
+    expect(arr.isEqualTo(brr)).toBe(true);
+    brr.put(1, 8);
+    console.log(`arr = ${arr}, brr = ${brr}`);
+    expect(arr.compareTo(brr)).toBe(LESS);
+    arr.put(3, 2);
+    console.log(`arr = ${arr}, brr = ${brr}`);
+    expect(arr.compareTo(brr)).toBe(UNRELATED);
+}
 
-    let m = new Map();
-    m.set(Number.NaN, 7);
-    m.set(Number.NaN, 6);
-    expect(m.size).toBe(1);
-    expect(m.get(Number.NaN)).toBe(6);
-    m.set(Number.NaN, Number.NaN);
-    expect(m.get(Number.NaN)).toBe(Number.NaN);
-    expect(Number.isNaN(m.get(Number.NaN))).toBe(true);
-    expect(m.size).toBe(1);
+test("AssocArray", () => {
+    testMutableMap(AssocArray());
 });
 
-test("JavaScript Behaviour: various", () => {
-    console.log("typeof primitives = ", typeof primitives);
-    expect(typeof primitives).toBe("object");
+test("HashMap", () => {
+    testMutableMap(HashMap());
+});
 
-    function isNumber(x : BigInt) : boolean {
-        try {
-            return x === BigInt(Number(x));
-        } catch {
-            return false;
+test("AssocArray", () => {
+    let a : Map<int, string | null> = AssocArrayFor(numbers, Anything);
+    a.set(1, "one");
+    a.set(2, null);
+    expect(a.get(1)).toBe("one");
+    expect(a.get(2)).toBe(null);
+    expect(a.get(3)).toBe(undefined);
+    console.log(`${a}`);
+});
+
+
+test("HashMap<number, number>", () => {
+    let a : Map<number, number> = HashMapFor(numbers, numbers);
+    let b : Map<number, number> = new Map();
+    let N = 1000;
+    for (let i = 0; i < N/2; i ++) {
+        let key = randomNat(N);
+        if (coinflip()) {
+            let value = randomNat();
+            a.set(key, value);
+            b.set(key, value);
+            expect(a.size).toBe(b.size);
+        } else {
+            let da = a.delete(key);
+            let db = b.delete(key);
+            expect(da).toBe(db);
+            expect(a.size).toBe(b.size);
         }
     }
+    for (let i = 0; i <= N; i ++) {
+        expect(a.get(i)).toBe(b.get(i));
+    }
+    //(a as any).get = 3;
+    console.log(`size of a = ${a.size}, size of b = ${b.size}`);
+});
 
+test("SpeedDemon native Map: number", () => {
+    let a : Map<number, number> = new Map();
+    let N = 1000000;
+    for (let i = 0; i < N/2; i ++) {
+        let key = randomNat(N);
+        if (coinflip()) {
+            let value = randomNat();
+            a.set(key, value);
+        } else {
+            let da = a.delete(key);
+        }
+    }
+    console.log(`size of a = ${a.size}`);
 });
 
 
+test("SpeedDemon number", () => {
+    let a : Map<number, number> = HashMapFor(numbers, numbers);
+    let N = 1000000;
+    for (let i = 0; i < N/2; i ++) {
+        let key = randomNat(N);
+        if (coinflip()) {
+            let value = randomNat();
+            a.set(key, value);
+        } else {
+            let da = a.delete(key);
+        }
+    }
+    console.log(`size of a = ${a.size}`);
+});
 
+test("SpeedDemon Num", () => {
+    let a : Map<Num, Num> = HashMap();
+    let N = 1000000;
+    for (let i = 0; i < N/2; i ++) {
+        let key = new Num(randomNat(N));
+        if (coinflip()) {
+            let value = new Num(randomNat());
+            a.set(key, value);
+        } else {
+            let da = a.delete(key);
+        }
+    }
+    console.log(`size of a = ${a.size}`);
+});
 
+test("SpeedDemon Int", () => {
+    let a : Map<Int, Int> = HashMap();
+    let N = 1000000;
+    for (let i = 0; i < N/2; i ++) {
+        let key = new Int(randomNat(N));
+        if (coinflip()) {
+            let value = new Int(randomNat());
+            a.set(key, value);
+        } else {
+            let da = a.delete(key);
+        }
+    }
+    console.log(`size of a = ${a.size}`);
+});
 
+test("SpeedDemon MutableInt", () => {
+    let a : Map<MutableInt, MutableInt> = HashMap();
+    let N = 1000000;
+    for (let i = 0; i < N/2; i ++) {
+        let key = new MutableInt(randomNat(N));
+        if (coinflip()) {
+            let value = new MutableInt(randomNat());
+            a.set(key, value);
+        } else {
+            let da = a.delete(key);
+        }
+    }
+    console.log(`size of a = ${a.size}`);
+});
+   
